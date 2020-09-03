daba7bde7e2fa6a449e6fafa0265221381f10b2f
supragya27 <supragya.chauhan001@gmail.com>
2020-09-03 21:37:25 +0530

    Updates

---
+++ precache-manifest.799bfd83c107ff11106783d838fe2ba0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad246b00baf372f6fedc361ffb21426f",
+    "url": "/My-Garden/index.html"
+  },
+  {
+    "revision": "b66832a966d8e73d22c6",
+    "url": "/My-Garden/static/css/main.a8b3032c.chunk.css"
+  },
+  {
+    "revision": "cb5d097d54df5fa80a3c",
+    "url": "/My-Garden/static/js/2.630c1387.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/My-Garden/static/js/2.630c1387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b66832a966d8e73d22c6",
+    "url": "/My-Garden/static/js/main.8e37d962.chunk.js"
+  },
+  {
+    "revision": "89dcbedb08910d205857",
+    "url": "/My-Garden/static/js/runtime-main.03496671.js"
+  },
+  {
+    "revision": "de75d22edd2057986581d9916f71d6fb",
+    "url": "/My-Garden/static/media/camellia.de75d22e.jpg"
+  },
+  {
+    "revision": "4e89c4f0800962ddbc39436a4f10c4bb",
+    "url": "/My-Garden/static/media/carnation.4e89c4f0.jpg"
+  },
+  {
+    "revision": "d01546cfd7b52b096f8e72081915dd6d",
+    "url": "/My-Garden/static/media/cyclamen.d01546cf.jpg"
+  },
+  {
+    "revision": "b6c46096adfd9d3f1482c5c2669c8d27",
+    "url": "/My-Garden/static/media/daisy.b6c46096.jpg"
+  },
+  {
+    "revision": "296b9715d3f03cbe0fd3ec3ad77b3bbe",
+    "url": "/My-Garden/static/media/forget-me-not.296b9715.jpg"
+  },
+  {
+    "revision": "aaebe924ae9ef0744d68c8a41f57fd64",
+    "url": "/My-Garden/static/media/iris.aaebe924.jpg"
+  },
+  {
+    "revision": "a515ec9cd8d9524ccc5abdeddaf7ff83",
+    "url": "/My-Garden/static/media/narcissus.a515ec9c.jpg"
+  },
+  {
+    "revision": "b06c69cd0e636536299721171f22d679",
+    "url": "/My-Garden/static/media/orchid.b06c69cd.jpg"
+  },
+  {
+    "revision": "d53c39913c8e6a547307a5fe6cf173e4",
+    "url": "/My-Garden/static/media/pansy.d53c3991.jpg"
+  },
+  {
+    "revision": "77ac273405c11d65a39e7e02a680e3bf",
+    "url": "/My-Garden/static/media/poppy.77ac2734.jpg"
+  },
+  {
+    "revision": "2943cfa8851f47228e94278a4bdff294",
+    "url": "/My-Garden/static/media/rose.2943cfa8.jpg"
+  },
+  {
+    "revision": "d3594c1352e7afd7c68231ed09e7498c",
+    "url": "/My-Garden/static/media/sunflower.d3594c13.jpg"
+  },
+  {
+    "revision": "ae1a73547117060166c0d1d8f9dc1e52",
+    "url": "/My-Garden/static/media/tulip.ae1a7354.jpg"
+  },
+  {
+    "revision": "c95e4d49d883bc115c41bfb03df7d569",
+    "url": "/My-Garden/static/media/violet.c95e4d49.jpg"
+  }
+]);
